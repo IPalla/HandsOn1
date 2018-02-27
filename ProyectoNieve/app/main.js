92a8cad92e03eeb386d173ef83be09ef31a2cf9d
Ignacio Pallares Jimenez <ignacio.pallares@babel.es>
2018-02-27 20:31:59 +0100

    Añadiendo JS al nav

--- ProyectoNieve/app/main.js
+++ ProyectoNieve/app/main.js
@@ -1,5 +1,21 @@
 export class Main{
     constructor(){
         console.log("Cargado");
+        this.vista = {
+            botonesMenu: document.querySelectorAll('nav ul li a'),
+            articleHtml: document.querySelector('article'), 
+            aImports: document.querySelectorAll('link[rel="import"]'),
+            oImports: {}
+        }
+        // Manejador de eventos del menu
+        this.vista.botonesMenu.forEach( (item) => {
+            item.addEventListener('click',this.menuItems.bind(this),true);
+            console.log("BInd");
+        })
+    }
+    menuItems(oEv) {
+        oEv.preventDefault();
+        console.dir(oEv.target.id);
     }
 }
+
