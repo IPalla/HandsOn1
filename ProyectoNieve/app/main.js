56e859b76df5ad571d574c62f09aee6cd15e969e
Ignacio Pallares Jimenez <ignacio.pallares@babel.es>
2018-02-28 13:08:26 +0100

    Validacion del formulario sin storage

--- ProyectoNieve/app/main.js
+++ ProyectoNieve/app/main.js
@@ -2,6 +2,7 @@ export class Main{
     constructor(){
         this.vista = {
             botonesMenu: document.querySelectorAll('nav ul li a'),          //Botones del menu
+            botonEnviar: null,                                              //Boton de enviar formulario. No se hace el selector hasta q se pulsa about
             articleHtml: document.querySelector('article'),
             templates: document.querySelectorAll('link[rel="import"]'),     //Templates importados
             keyTemplates: {}                                                //Array indexada, clave= id del boton, valor= template
@@ -11,6 +12,12 @@ export class Main{
             item.addEventListener('click',this.menuItems.bind(this),true);
             this.bindTemplates(item);
         })
+        this.form={
+            nombre: null,
+            email: null,
+            preferencia: null,
+            comentario: null
+        }
     }
     /*
          Si un botón es pulsado carga la template correspondiente a su valor
@@ -33,6 +40,11 @@ export class Main{
             this.vista.articleHtml.innerHTML="<h2>Ocurrio un error cargando el template</h2>";
         }
         this.vista.articleHtml.innerHTML=toInyect.innerHTML;
+
+        this.vista.botonEnviar=document.querySelector('button');
+        if(this.vista.botonEnviar){ //Si existe boton enviar (about) se añade el event listener
+            this.vista.botonEnviar.addEventListener('click', this.botonFormulario.bind(this));
+        }
     }
     /*
         Esta funcion es la encargada de crear el array asociativo. Los templates deben tener como título el id
@@ -50,5 +62,28 @@ export class Main{
             this.vista.keyTemplates[button.id]=this.vista.templates[0].import;
         }
     }
-}
+    botonFormulario(oEv){
+        oEv.preventDefault();
+        let nombre=document.getElementById("Nombre").value;
+        let email=document.getElementById("Email").value;
+        let comentario=document.getElementById("Comentario").value;
+        let esqui=document.getElementById("Preferencia_esqui").checked;
+        let snow=document.getElementById("Preferencia_snow").checked;
+        if(comentario.length < 5){
+            document.getElementById("Comentario").placeholder="Mínimo 5 caracteres";
+            document.getElementById("Comentario").className='required';
+        }
+        else{
+            document.getElementById("Comentario").className='';
+        }
+        console.dir(esqui);
+        console.dir(snow);
+        if(!esqui && !snow){
+            document.getElementById("Preferencia").className="required";
+        }
+        else{
+            document.getElementById("Preferencia").className="";
+        }
 
+    }
+}
